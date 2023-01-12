9653b310e2b206bccf4a82a641b15716f43e6dd7
chughes271 <chhughes@mtu.edu>
2023-01-12 07:09:39 -0600

    Made getValue method less buggy accounting for boundary conditions. Added deck class to create and shuffle a deck of cards. Created a hand class to create a blank hand then add cards from the deck and count the score.

--- BlackJack.py
+++ BlackJack.py
@@ -3,66 +3,90 @@ import random
 
 class card:
     def __init__(self,name,suit):
+        #Variables in each instance
         self.name = name
         self.suit = suit
         self.value = 0
-
+        self.label = ""
         #get value of card
         self.getValue()
-        #paackage characteristics of instance in label
-        self.label = f"{self.name} of {self.suit} with score of {self.value}"
+
 
     def __str__(self):
         return(self.label)
 
     #method to get numerical value of the card instance
     def getValue(self):
-        if self.name == "10" or self.name in "AJQK":
-            self.value = 10
-        elif int(self.name) >0 and int(self.name) <10:
+        try:
             self.value = int(self.name)
+            if self.value >=2 and self.value <=10:
+                self.label = f"{self.name} of {self.suit}" # with score of {self.value}"
+            else:
+                self.label = "Incorrect Input, please input the correct card info"
+
+        except:
+            if self.name in "AJQKajqk":
+                self.value = 10
+                self.label = f"{self.name} of {self.suit}" # with score of {self.value}"
+            else:
+                self.label = "Incorrect Input, please input the correct card info"
+
+class deck:
+
+    def __init__(self):
+        self.deckOfCards = []
+        self.dealCard = []
+
+        self.createDeck()
+        self.shuffleDeck()
+
+    #Used for testing - Temporary  Not sure the proper way of doing this. I created one return, but mainly used it for testing
+    def __str__(self):
+        return ', '.join(str(cards) for cards in self.deckOfCards)
+        #return self.dealCard
+
+    def createDeck(self):
+        suits = ["Hearts", "Spades", "Diamonds", "Clubs"]
+        names = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+        for s in suits:
+            for n in names:
+                test = card(n,s)
+                self.deckOfCards.append(test)
+
+    def shuffleDeck(self):
+        random.shuffle(self.deckOfCards)
+
+    #def dealingCard(self):
+        #self.dealCard = self.deckOfCards.pop()
+
+class hand:
+
+    def __init__(self,currentDeck):
+        self.deck = currentDeck
+        self.cardsInHand = []
+        self.totalScore = 0
+
+    def __str__(self,):
+        return ', '.join(str(c) for c in self.cardsInHand)
+
+    def addCard(self):
+        self.cardsInHand.append(self.deck.deckOfCards.pop())
+
+    def handScore(self):
+        self.totalScore = 0
+        for card in self.cardsInHand:
+            self.totalScore = self.totalScore + card.value
+        return self.totalScore
 
 
 if __name__ == '__main__':
-    card1 = card('10','Spades')
-    print(card1)
-
-
-
-## Future failed code unready
-
-# class deck:
-#     def __init__(self):
-#         self.cards = []
-#         self.buildDeck()
-#         self.shuffleDeck()
-#
-#
-#     def buildDeck(self):
-#         suitVec = ["Heart", "Spades", "Diamons", "Clubs"]
-#         nameVec = ["02", "03", "04", "05", "06", "07", "08", "09", "10", "J ", "Q ", "K ", "A "]
-#
-#         for suit in suitVec:
-#             for name in nameVec:
-#                 self.cards.append(card(name,suit))
-#
-#     def shuffleDeck(self):
-#         random.shuffle(self.cards)
-#
-#     def dealCard(self):
-#         return self.cards.pop()
-#
-# class hand:
-#
-#     def __init__(self):
-#         self.cards = []
-#         self.addingCard(deck().dealCard())
-#         self.cardValue = 0
-#         self.score = 0
-#
-#     def __str__(self):
-#         return ', '.join(str(c) for c in self.cards) ## Why cant I just return self.cards or str(self.cards).... I have to str() each letter in the self.cards to get english output
-#
-#     def addingCard(self,card):
-#         self.cards.append(card)
-##
+    theDeck = deck()
+    print(theDeck)
+    myHand = hand(theDeck)
+    myHand.addCard()
+    print(myHand)
+    print(myHand.handScore())
+    myHand.addCard()
+    print(myHand)
+    print(myHand.handScore())
