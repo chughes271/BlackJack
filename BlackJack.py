282b97cb3781a47aa42a7677af50285836254ca8
chughes271 <chhughes@mtu.edu>
2023-01-14 20:10:33 -0600

    Made aces 11pts, created function gameClosing and gameEnd to play the game out when a player chooses to stand and to check/execute end of game. Game works other than choosing ace value and splitting hands.

--- BlackJack.py
+++ BlackJack.py
@@ -25,9 +25,12 @@ class card:
                 self.label = "Incorrect Input, please input the correct card info"
 
         except:
-            if self.name in "AJQKajqk":
+            if self.name in "JQKjqk":
                 self.value = 10
                 self.label = f"{self.name} of {self.suit}" # with score of {self.value}"
+            elif self.name in "Aa":
+                self.value = 11
+                self.label = f"{self.name} of {self.suit}"  # with score of {self.value}"
             else:
                 self.label = "Incorrect Input, please input the correct card info"
 
@@ -92,27 +95,33 @@ class handOptions:
         self.currentHand.addCard(theDeck)
 
     def split(self):
-        self.hand2 = hand()
-        self.hand1 = self.currentHand
-        self.hand2.addCard(self.hand1)
+        if self.currentHand.cardsInHand[0] == self.currentHand.cardsInHand[1]:
 
-        return(self.hand1,self.hand2)
+            self.hand2 = hand()
+            self.hand1 = self.currentHand
+            self.hand2.addCard(self.hand1)
+
+            return(self.hand1,self.hand2)
+        else:
+            print("Cannot split your hand")
+            self.askAgain = askPlayer(options)
+            return self.askAgain
 
     def decideAce(self):
         pass
 
 def askPlayer(handoption1):
     playerInput = input("input your move")
+    status = False
     if playerInput == "0":                 #Stand and proceed
-        return handoption1.currentHand
+        status = gameClosing()
+        return(handoption1.currentHand,status)
     elif playerInput == "1":
         handoption1.hit()
-        return handoption1.currentHand
+        return(handoption1.currentHand,status)
     elif playerInput == "2":
-        options.split()
-        split1 = handoption1[0]
-        split2 = handoption1[1]
-        return split1,split2
+        handoption1 = options.split()
+        return(handoption1)
     else:
         print("Not valid")
 
@@ -122,6 +131,49 @@ def gamePrep():
     myHand.addCard(theDeck)
     dealerHand.addCard(theDeck)
 
+def gameClosing():
+    while(dealerHand.handScore() < 21 and dealerHand.handScore() <= myHand.handScore()):
+        dealerHand.addCard(theDeck)
+        #print(f'Dealers hand = {dealerHand}')
+    #end = gameEnd(myHand.handScore(),dealerHand.handScore(),True)
+
+    return(True)
+
+
+def gameEnd(player,dealer,status):
+    #print(player,dealer,status)
+
+    endGame = False
+    win = False
+    push = False
+
+    if player > 21:
+        endGame = True
+    else:
+        if dealer > 21:
+            endGame = True
+            win = True
+        elif dealer == 21:
+            endGame = True
+        elif player == 21:
+            status = gameClosing()
+            dealer = dealerHand.handScore()
+    if status == True:
+        endGame = True
+        if player > dealer or dealer > 21:
+            win = True
+        elif player == dealer:
+            push = True
+
+    if endGame == True:
+        if win == True:
+            print(f"\nYou won {player}-{dealer}\nYour hand: {myHand}\nDealer's hand: {dealerHand}")
+        elif push == True:
+            print(f"\nGame pushed {player}-{dealer}\nYour hand: {myHand}\nDealer's hand: {dealerHand}")
+        else:
+            print(f"\nYou lost {player}-{dealer}\nYour hand: {myHand}\nDealer's hand: {dealerHand}")
+
+    return(endGame)
 
 if __name__ == '__main__':
     theDeck = deck()
@@ -134,4 +186,8 @@ if __name__ == '__main__':
         print(f'Your hand = {myHand}')
 
         options = handOptions(myHand)
-        myHand = askPlayer(options)
+        #checkaces
+        myHand,status = askPlayer(options)
+        end = gameEnd(myHand.handScore(), dealerHand.handScore(),status)
+
+
